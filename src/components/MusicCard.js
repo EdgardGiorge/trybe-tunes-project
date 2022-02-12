2a9864aa44e16d1a0eb03a5a7a135f597cb2386e
EdgardGiorge <edgardgiorge@hotmail.com>
2022-02-12 18:01:42 -0300

    Requisito 8

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,25 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Loading from './Loading';
+import Card from './Card';
+import { addSong } from '../services/favoriteSongsAPI';
 
 // Ref. Estudo em grupo em sala de estudos
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      favoriteMusic: {},
+      isLoading: false,
+    };
+  }
+
+  createObj = () => {
+    const { musicsAlbum } = this.props;
+    const { favoriteMusic } = this.state;
+    const favSongs = Object.values(favoriteMusic);
+    const musicaKeys = Object.keys(favoriteMusic);
+
+    const array = musicaKeys.map(
+      (elemento, index) => ({ [elemento]: favSongs[index] }),
+    );
+
+    const valid = array.filter(
+      (element, index) => element[musicaKeys[index]] === true,
+    );
+
+    const music = valid.map((musica) => Object.keys(musica));
+
+    const last = music.map((atLast) => atLast[0]);
+
+    const favorite = [];
+
+    for (let index = 0; index < last.length; index += 1) {
+      for (let i = 0; i < musicsAlbum.length; i += 1) {
+        if (last[index] === musicsAlbum[i].trackName) {
+          favorite.push(musicsAlbum[i]);
+        }
+      }
+    }
+    favorite.forEach((song) => this.adicionaMusica(song));
+  }
+
+  adicionaMusica = async (song) => {
+    this.setState({ isLoading: true });
+    await addSong(song);
+    this.setState({ isLoading: false });
+  }
+
+  favoriteSong = ({ target }) => {
+    const { name, checked } = target;
+
+    this.setState((prevState) => (
+      { favoriteMusic: { ...prevState.favoriteMusic, [name]: checked } }),
+    () => this.createObj());
+  }
+
   render() {
     const { musicsAlbum } = this.props;
+    const { favoriteMusic, isLoading } = this.state;
     return (
       <div>
-        {musicsAlbum.map(({ trackName, previewUrl, trackId }) => (
-          <div key={ trackId }>
-            <p>{trackName}</p>
-            <audio data-testid="audio-component" src={ previewUrl } controls>
-              <track kind="captions" />
-              O seu navegador não suporta o elemento
-              {' '}
-              <code>audio</code>
-              .
-            </audio>
-          </div>
-        ))}
+        {isLoading ? <Loading /> : <Card
+          musics={ musicsAlbum }
+          checked={ favoriteMusic }
+          onChange={ this.favoriteSong }
+        />}
       </div>
     );
   }
